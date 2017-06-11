037a4f12e3a9b9eb8c8553ab92693378ebe29f60
Jaskirat Singh <jaskiratsingh035@gmail.com>
2017-06-11 14:16:07 +0530

    all file added

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count = 0
+    @IBOutlet var label : UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func incrementCount() {
+        self.count = self.count+1
+        self.label.text = "\(self.count)"
     }
-
 
 }
 
